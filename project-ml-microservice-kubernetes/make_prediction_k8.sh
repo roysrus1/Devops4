869cd1b4531a65888e383e96a49d4ba52f7681e1
EC2 Default User <ec2-user@ip-172-31-57-190.us-west-2.compute.internal>
2020-05-16 22:16:38 +0000

    Version Saturday May 16

---
+++ project-ml-microservice-kubernetes/make_prediction_k8.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+echo "**** Running Prediction ****"
+HOST=10.102.162.34
+PORT=8000
+echo "Host and Port: $HOST  $PORT"
+# POST method predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://localhost:$PORT/predict
