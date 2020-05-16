edfbadc8c8fdc0e5ceb1707ba9804ef0b018b664
EC2 Default User <ec2-user@ip-172-31-57-190.us-west-2.compute.internal>
2020-05-16 22:17:56 +0000

    Version Saturday May 16

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,15 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=docker.io/roysrus1/devops:app
+dockerpath=docker.io/roysrus1/app:v2
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment app1 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
 
+kubectl get pods
+
 # Step 4:
 # Forward the container port to a host
 
+kubectl expose deployment app1 --type=LoadBalancer --port=8000 --target-port=80
+minikube service app1
+
+kubectl describe service app1
