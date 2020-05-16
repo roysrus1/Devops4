16479dd7dfb195d01b553fce929060f36c95dd71
EC2 Default User <ec2-user@ip-172-31-57-190.us-west-2.compute.internal>
2020-05-16 02:29:22 +0000

    Final

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,6 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=docker.io/roysrus1/devops:app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=roysrus1/app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login
+docker image tag app "${dockerpath}:v2"
+
+
 # Step 3:
 # Push image to a docker repository
+
+docker push $dockerpath
